2689f308a09ed84801f404049b380a041506fab4
GeniusMiao <miaozijianzz@outlook.com>
2020-07-12 20:42:27 +0800

    update mysql

--- mysql/插入或导入数据.sql
+++ mysql/插入或导入数据.sql
@@ -1,5 +1,5 @@
 -- 导入csv文件中的数据至member_value_table表
-load data infile 'C:/Users/miaoz/Desktop/NEU Soft Big Data/AirLine/mysql_data_in/air_data.csv'
-into table member_value_table
+load data infile 'C:/Users/miaoz/Desktop/NEU Soft Big Data/AirLine/data_in/air_data_in.csv'
+into table `airline`.`member_value_table`
 fields terminated by ',' optionally enclosed by '"' escaped by '"'
 lines terminated by '\r\n';
